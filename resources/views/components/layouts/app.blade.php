0fc36d4c491fe8c0ceb31dbe09cf6556ccc00ca9
INDRATHEDESTROYER <D308873@edu.curio.nl>
2025-09-12 10:11:52 +0200

    update footer

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -2,31 +2,31 @@
 <html lang="en">
 <head>
     <x-head/>
+    <style>
+      body {
+        margin: 0; /*  niet veranderen pls, het is super fragiel :(  */
+      }
+      .footer {
+        background-color: red;
+        padding: 50px;
+        width: 100%;
+      }
+    </style>
 </head>
 <body>
-    <style>
-.footer {
-  background-color: red;
-  padding: 50px;
-  width: 100%;
-  position: fixed;
-  left: 0;
-  bottom: 0;
-}
-
-
-    </style>
 
 <x-navbar/>
+
 <div class="container">
     <div class="row">
-
         <div class="col-md-8">
             <x-header/>
 
             <ul class="breadcrumb">
-                <li><a href="/" title="{{ __('misc.home_alt') }}"
-                       alt="{{ __('misc.home_alt') }}">{{ __('misc.home') }}</a></li>
+                <li>
+                  <a href="/" title="{{ __('misc.home_alt') }}"
+                     alt="{{ __('misc.home_alt') }}">{{ __('misc.home') }}</a>
+                </li>
                 {{ $breadcrumb ?? '' }}
             </ul>
 
@@ -38,28 +38,22 @@
 
             <ul class="breadcrumb">
                 <li>
-                    <a href="/" title="{{ __('misc.home_alt') }}" alt="{{ __('misc.home_alt') }}">{{ __('misc.home') }}</a>
+                    <a href="/" title="{{ __('misc.home_alt') }}"
+                       alt="{{ __('misc.home_alt') }}">{{ __('misc.home') }}</a>
                 </li>
                 {{ $breadcrumb ?? '' }}
             </ul>
-
         </div>
-
     </div> <!-- einde row -->
-
-    <div class="footer">
-        <x-footer>
-
-        </x-footer>
-    </div>
-
 </div> <!-- einde container -->
 
-<!-- Bootstrap core JavaScript
-================================================== -->
-<!-- Placed at the end of the document so the pages load faster -->
+<!-- footer buiten container -->
+<div class="footer">
+    <x-footer></x-footer>
+</div>
+
+<!-- JS -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-<script>//window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
 <script src="{{ asset('/js/app.js') }}"></script>
 
 </body>
