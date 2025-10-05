c81e8161bdcb8556b32cff161839f372bc171736
INDRATHEDESTROYER <D308873@edu.curio.nl>
2025-10-05 19:25:59 +0200

    Revert "breadcrumb verwijderd"
    This reverts commit ca859d7e44efd07ef9c8f56dadf1ff78c6001372.

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -74,6 +74,14 @@
                 {{ $slot }}
             @endif
 
+            <ul class="breadcrumb">
+                <li>
+                    <a href="/" title="{{ __('misc.home_alt') }}"
+                       alt="{{ __('misc.home_alt') }}">{{ __('misc.home') }}</a>
+                </li>
+                {{ $breadcrumb ?? '' }}
+
+            </ul>
         </div>
     </div>
 </div>
