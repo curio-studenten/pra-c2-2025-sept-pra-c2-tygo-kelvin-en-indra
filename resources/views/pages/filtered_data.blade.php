e6bc625235d2c49dff414f0aefa3373f8ff0a092
Ty <d292003@edu.curio.nl>
2025-10-07 14:54:35 +0200

    Finished ticket 13
    This concludes my work for PRA, if my help is needed feel free to message me. But I won't work on it anymore

--- resources/views/pages/filtered_data.blade.php
+++ resources/views/pages/filtered_data.blade.php
@@ -1,30 +1,55 @@
-<x-layouts.app >
-<x-filtered-data />
-<div class="brands-list">
+<x-layouts.app>
+    <x-filtered-data />
     @php
         $brands = \App\Models\Brand::orderBy('name')->get();
         $alphabet = range('A', 'Z');
+        $lettersWithBrands = [];
+
+        foreach ($alphabet as $letter) {
+            $hasBrand = false;
+
+            foreach ($brands as $brand) {
+                if (strtoupper(substr($brand->name, 0, 1)) == $letter) {
+                    $hasBrand = true;
+                    break;
+                }
+            }
+
+            if ($hasBrand) {
+                $lettersWithBrands[] = $letter;
+            }
+        }
+
+
+        $lettersWithBrands = array_chunk($lettersWithBrands, 3);
     @endphp
+    <div class="brands-list">
+        @foreach($lettersWithBrands as $letters)
+    <div class="brands-row">
+        @foreach($letters as $letter)
+            @php
+                $brandsForLetter = [];
+                foreach ($brands as $brand) {
+                    if (strtoupper(substr($brand->name, 0, 1)) == $letter) {
+                        $brandsForLetter[] = $brand;
+                    }
+                }
+            @endphp
 
-    @foreach($alphabet as $letter)
-        @php
-            $brandsForLetter = $brands->filter(function($brand) use ($letter) {
-                return (substr($brand->name, 0, 1)) == $letter;
-            });
-        @endphp
-
-        @if($brandsForLetter->isNotEmpty())
-            <h2 id="letter-{{ $letter }}">{{ $letter }}</h2>
-            <ul>
-                @foreach($brandsForLetter as $brand)
-                    <li>
-                    <a href="/{{ $brand->id }}/{{ $brand->getNameUrlEncodedAttribute() }}/">
-                        <button>{{ $brand->name }}</button>
-                    </a>
-                </li>
-                @endforeach
-            </ul>
-        @endif
-    @endforeach
-</div>
+            <div class="brand-column">
+                <h2 id="letter-{{ $letter }}">{{ $letter }}</h2>
+                <ul>
+                    @foreach($brandsForLetter as $brand)
+                        <li>
+                            <a href="/{{ $brand->id }}/{{ $brand }}/">
+                                <button>{{ $brand->name }}</button>
+                            </a>
+                        </li>
+                    @endforeach
+                </ul>
+            </div>
+        @endforeach
+    </div>
+@endforeach
+    </div>
 </x-layouts.app>
