7623bec8b79c5f12b883c3a9ce8706b992c5f89e
Ty <d292003@edu.curio.nl>
2025-10-07 15:02:56 +0200

    fixed footer in filtered data

--- resources/views/pages/filtered_data.blade.php
+++ resources/views/pages/filtered_data.blade.php
@@ -25,31 +25,31 @@
     @endphp
     <div class="brands-list">
         @foreach($lettersWithBrands as $letters)
-    <div class="brands-row">
-        @foreach($letters as $letter)
-            @php
-                $brandsForLetter = [];
-                foreach ($brands as $brand) {
-                    if (strtoupper(substr($brand->name, 0, 1)) == $letter) {
-                        $brandsForLetter[] = $brand;
+        <div class="brands-row">
+            @foreach($letters as $letter)
+                @php
+                    $brandsForLetter = [];
+                    foreach ($brands as $brand) {
+                        if (strtoupper(substr($brand->name, 0, 1)) == $letter) {
+                            $brandsForLetter[] = $brand;
+                        }
                     }
-                }
-            @endphp
+                @endphp
 
-            <div class="brand-column">
-                <h2 id="letter-{{ $letter }}">{{ $letter }}</h2>
-                <ul>
-                    @foreach($brandsForLetter as $brand)
-                        <li>
-                            <a href="/{{ $brand->id }}/{{ $brand }}/">
-                                <button>{{ $brand->name }}</button>
-                            </a>
-                        </li>
-                    @endforeach
-                </ul>
-            </div>
-        @endforeach
-    </div>
-@endforeach
-    </div>
+                <div class="brand-column">
+                    <h2 id="letter-{{ $letter }}">{{ $letter }}</h2>
+                    <ul>
+                        @foreach($brandsForLetter as $brand)
+                            <li>
+                                <a href="/{{ $brand->id }}/{{ $brand }}/">
+                                    <button>{{ $brand->name }}</button>
+                                </a>
+                            </li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endforeach
+        </div>
+    @endforeach
+</div>
 </x-layouts.app>
